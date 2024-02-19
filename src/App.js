2fc3e4c264dd8d669708e67c5cee589ad0b4b23e
rashidpbi <rashidpbi786@gmail.com>
2024-02-19 15:03:43 +0530

    starter files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Fetch from "./api/Fetch";
+export default function App(){
+    
+    return(<div >
+      <Fetch/>
+        </div>);
 }
 
-export default App;
+
+//to do App
+//header mylist
+//shows list
+//add
+//delete
---
+++ src/api/Fetch.js
@@ -0,0 +1,10 @@
+function Fetch(){
+return (<div >
+    <h1 class="text-3xl font-bold underline">
+    hello world
+  </h1>
+ 
+
+</div>)
+}
+export default Fetch;
